7ebe7cc946a9b3ca4661c545ed081c5350b2b028
Merlin <36685500+Merlin-san@users.noreply.github.com>
2020-02-25 10:26:38 -0800

    Add emulation for Udon functions to UdonSharpBehaviour
    - Add emulation for GetProgramVariable, SetProgramVariable, and SendCustomEvent

--- Assets/UdonSharp/Scripts/UdonSharpBehaviour.cs
+++ Assets/UdonSharp/Scripts/UdonSharpBehaviour.cs
@@ -1,16 +1,49 @@
-﻿using UnityEngine;
+﻿using System.Linq;
+using System.Reflection;
+using UnityEngine;
 using VRC.Udon.Common.Interfaces;
 
 namespace UdonSharp
 {
     public abstract class UdonSharpBehaviour : MonoBehaviour
     {
-        // Stubs for the UdonBehaviour functions
-        public object GetProgramVariable(string name) { return null; }
-        public void SetProgramVariable(string name, object value) { }
-        public void SendCustomEvent(string eventName) { }
+        // Stubs for the UdonBehaviour functions that emulate Udon behavior
+        public object GetProgramVariable(string name)
+        {
+            FieldInfo variableField = GetType().GetField(name, BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
+
+            if (variableField == null)
+                return null;
+
+            return variableField.GetValue(this);
+        }
+
+        public void SetProgramVariable(string name, object value)
+        {
+            FieldInfo variableField = GetType().GetField(name, BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
+
+            if (variableField != null)
+            {
+                variableField.SetValue(this, value);
+            }
+        }
+
+        public void SendCustomEvent(string eventName)
+        {
+            MethodInfo eventmethod = GetType().GetMethods(BindingFlags.Public | BindingFlags.Instance).Where(e => e.Name == eventName && e.GetParameters().Length == 0).FirstOrDefault();
+
+            if (eventmethod != null)
+            {
+                eventmethod.Invoke(this, new object[] { });
+            }
+        }
+
         public void SendCustomNetworkEvent(NetworkEventTarget target, string eventName) { }
-        public static GameObject VRCInstantiate(GameObject original) { return Instantiate(original); }
+
+        public static GameObject VRCInstantiate(GameObject original)
+        {
+            return Instantiate(original);
+        }
 
         // Method stubs for auto completion
         public virtual void Interact() { }
