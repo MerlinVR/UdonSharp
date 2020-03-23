43c1d595a58bf75b3e1a71a4a27b208353be08c3
Merlin <36685500+Merlin-san@users.noreply.github.com>
2020-03-22 19:44:48 -0700

    Add logging that tells what script and line caused a runtime exception
    - Add runtime exception logging that finds the script responsible for a runtime exception and the line in that script that the exception was thrown from.
    - Add handling for parsing the output logs while the VRC client is running to look for Udon exceptions, but the program counter and heap dump have been removed from the client exception logs so this handling is disabled until VRC re-adds the in-game debug info needed.

--- Assets/UdonSharp/Editor/UdonSharpClassDebugInfo.cs
+++ Assets/UdonSharp/Editor/UdonSharpClassDebugInfo.cs
@@ -13,15 +13,22 @@ namespace UdonSharp
         [System.Serializable]
         public class DebugLineSpan
         {
-            public int StartInstruction { get; set; } = 0;
-            public int EndInstruction { get; set; } = 0;
+            public int startInstruction = 0;
+            public int endInstruction = 0;
 
-            public int StartSourceChar { get; set; } = 0;
-            public int EndSourceChar { get; set; } = 0;
+            public int startSourceChar = 0;
+            public int endSourceChar = 0;
+
+            public int line = 0;
+            public int lineChar = 0;
+
+            public string spanCodeSection = "";
         }
 
         [UnityEngine.SerializeField]
         private DebugLineSpan[] serializedDebugSpans;
+
+        public DebugLineSpan[] DebugLineSpans { get { return serializedDebugSpans; } }
 
         // Purposefully won't be serialized
         public AssemblyBuilder assemblyBuilder { get; set; }
@@ -53,18 +60,19 @@ namespace UdonSharp
 
             DebugLineSpan lastLineSpan = debugSpans.Last();
 
-            lastLineSpan.EndInstruction = assemblyBuilder.programCounter - 1;
-            lastLineSpan.EndSourceChar = node.SpanStart;
+            lastLineSpan.endInstruction = assemblyBuilder.programCounter - 1;
+            lastLineSpan.endSourceChar = node.SpanStart;
+            lastLineSpan.spanCodeSection = sourceText.Substring(lastLineSpan.startSourceChar, lastLineSpan.endSourceChar - lastLineSpan.startSourceChar);
 
             DebugLineSpan nextLineSpan = new DebugLineSpan();
-            nextLineSpan.StartInstruction = assemblyBuilder.programCounter;
-            nextLineSpan.StartSourceChar = node.SpanStart;
+            nextLineSpan.startInstruction = assemblyBuilder.programCounter;
+            nextLineSpan.startSourceChar = node.SpanStart;
 
             debugSpans.Add(nextLineSpan);
 
             if (includeInlineCode)
             {
-                int lineStart = nextLineSpan.StartSourceChar;
+                int lineStart = nextLineSpan.startSourceChar;
 
                 for (; lineStart > 0 && sourceText[lineStart] != '\n' && sourceText[lineStart] != '\r'; --lineStart) { }
 
@@ -72,7 +80,7 @@ namespace UdonSharp
                 {
                     List<string> spanCodeLines = new List<string>();
 
-                    for (int idx = nextLineSpan.StartSourceChar; idx < sourceText.Length; ++idx)
+                    for (int idx = nextLineSpan.startSourceChar; idx < sourceText.Length; ++idx)
                     {
                         if (sourceText[idx] == '\n' || sourceText[idx] == '\r')
                         {
@@ -99,9 +107,37 @@ namespace UdonSharp
         {
             serializedDebugSpans = new DebugLineSpan[debugSpans.Count];
 
+            int lastStart = 0;
+            int lastLineCount = 0;
+            int lastCharCount = 0;
             for (int i = 0; i < serializedDebugSpans.Length; ++i)
             {
                 serializedDebugSpans[i] = debugSpans[i];
+                DebugLineSpan span = serializedDebugSpans[i];
+                if (span.endInstruction <= 0 && span.startInstruction > 0)
+                    span.endInstruction = span.startInstruction;
+
+                if (span.endSourceChar == 0 && span.startSourceChar > 0)
+                    span.endSourceChar = span.startSourceChar;
+
+                int lineCount = lastLineCount;
+                int lineChar = lastCharCount;
+                for (int j = lastStart; j < serializedDebugSpans[i].startSourceChar; ++j)
+                {
+                    ++lineChar;
+                    if (sourceText[j] == '\n')
+                    {
+                        ++lineCount;
+                        lineChar = 0;
+                    }
+                }
+
+                lastCharCount = lineChar;
+                lastLineCount = lineCount;
+                lastStart = span.startSourceChar;
+
+                serializedDebugSpans[i].line = lineCount;
+                serializedDebugSpans[i].lineChar = lineChar;
             }
         }
     }
