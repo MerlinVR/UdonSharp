46342da1b2d5e4507e870692602c379974221df7
Merlin <36685500+Merlin-san@users.noreply.github.com>
2020-04-07 11:26:03 -0700

    Fix calling methods on user types stored in arrays
    - Fix calling methods on user types stored in arrays
    - Change some LINQ expressions to plain Finds

--- Assets/UdonSharp/Editor/UdonSharpExpressionCapture.cs
+++ Assets/UdonSharp/Editor/UdonSharpExpressionCapture.cs
@@ -1855,12 +1855,12 @@ namespace UdonSharp
 
             System.Type returnType = GetReturnType(true);
 
-            ClassDefinition externClass = visitorContext.externClassDefinitions.Where(e => e.userClassType == returnType).FirstOrDefault();
+            ClassDefinition externClass = visitorContext.externClassDefinitions.Find(e => e.userClassType == returnType);
 
             if (externClass == null)
                 return false;
 
-            FieldDefinition foundDefinition = externClass.fieldDefinitions.Where(e => e.fieldSymbol.symbolOriginalName == fieldToken && e.fieldSymbol.declarationType.HasFlag(SymbolDeclTypeFlags.Public)).FirstOrDefault();
+            FieldDefinition foundDefinition = externClass.fieldDefinitions.Find(e => e.fieldSymbol.symbolOriginalName == fieldToken && e.fieldSymbol.declarationType.HasFlag(SymbolDeclTypeFlags.Public));
 
             if (foundDefinition == null)
                 return false;
@@ -1879,12 +1879,13 @@ namespace UdonSharp
             if (accessSymbol == null || !accessSymbol.IsUserDefinedBehaviour())
                 return false;
 
-            ClassDefinition externClass = visitorContext.externClassDefinitions.Where(e => e.userClassType == accessSymbol.userCsType).FirstOrDefault();
+            System.Type returnType = GetReturnType(true);
+            ClassDefinition externClass = visitorContext.externClassDefinitions.Find(e => e.userClassType == returnType);
 
             if (externClass == null)
                 return false;
 
-            MethodDefinition foundDefinition = externClass.methodDefinitions.Where(e => e.originalMethodName == methodToken && e.declarationFlags.HasFlag(MethodDeclFlags.Public)).FirstOrDefault();
+            MethodDefinition foundDefinition = externClass.methodDefinitions.Find(e => e.originalMethodName == methodToken && e.declarationFlags.HasFlag(MethodDeclFlags.Public));
 
             if (foundDefinition == null)
                 return false;
