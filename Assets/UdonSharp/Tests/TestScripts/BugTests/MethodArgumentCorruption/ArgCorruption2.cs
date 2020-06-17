bc786bb2c76e5e8ca389080f55a6887a08c617b2
Merlin <36685500+Merlin-san@users.noreply.github.com>
2020-06-16 22:12:23 -0700

    Add tests for variables being assigned incorrectly
    - Add tests for #41

---
+++ Assets/UdonSharp/Tests/TestScripts/BugTests/MethodArgumentCorruption/ArgCorruption2.cs
@@ -0,0 +1,14 @@
+
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+namespace UdonSharp.Tests
+{
+    [AddComponentMenu("Udon Sharp/Tests/ArgCorruption2")]
+    public class ArgCorruption2 : UdonSharpBehaviour
+    {
+        public ArgCorruption3 udon;
+    }
+}
