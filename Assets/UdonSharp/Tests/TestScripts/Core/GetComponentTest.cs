6b554f35a4b62d33658de5c7420a7fb32444e3f7
Merlin <36685500+Merlin-san@users.noreply.github.com>
2020-06-18 17:59:19 -0700

    Add more GetComponent tests
    - Add more GetComponent tests to verify regression in beta client

--- Assets/UdonSharp/Tests/TestScripts/Core/GetComponentTest.cs
+++ Assets/UdonSharp/Tests/TestScripts/Core/GetComponentTest.cs
@@ -13,6 +13,8 @@ namespace UdonSharp.Tests
         [System.NonSerialized]
         public IntegrationTestSuite tester;
 
+        public GameObject modObject;
+
         public void ExecuteTests()
         {
             tester.TestAssertion("GetComponent<Transform>()", GetComponent<Transform>() != null);
@@ -27,7 +29,25 @@ namespace UdonSharp.Tests
 
             tester.TestAssertion("GetComponentsInChildren<PlayerModSetter>()", modSetters.Length == 3);
             
-            tester.TestAssertion("GetComponentsInChildren<MeshRenderer>()", GetComponentsInChildren<MeshRenderer>().Length == 2); 
+            tester.TestAssertion("GetComponentsInChildren<MeshRenderer>()", GetComponentsInChildren<MeshRenderer>().Length == 2);
+
+            UdonSharpBehaviour getBehaviour = (UdonSharpBehaviour)modObject.GetComponent(typeof(UdonBehaviour));
+
+            tester.TestAssertion("Get UdonBehaviour typeof(UdonBehaviour)", getBehaviour != null);
+
+            long typeID = GetUdonTypeID<PlayerModSetter>();
+
+            tester.TestAssertion("Type ID matches", typeID == getBehaviour.GetUdonTypeID());
+
+            tester.TestAssertion("Correct number of UdonBehaviours on gameobject", modObject.GetComponents(typeof(UdonBehaviour)).Length == 4);
+
+            //Debug.Log(getBehaviour.GetUdonTypeID());
+            //Debug.Log(getBehaviour.GetUdonTypeName());
+
+            //foreach (Component behaviour in modObject.GetComponents(typeof(UdonBehaviour)))
+            //{
+            //    Debug.Log("Component name: " + ((UdonSharpBehaviour)behaviour).GetUdonTypeName());
+            //}
         }
     }
 }
