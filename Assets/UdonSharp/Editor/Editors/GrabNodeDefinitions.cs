503220078fa67586e13e3d077896e6749f68effe
Merlin <36685500+Merlin-san@users.noreply.github.com>
2020-07-11 14:12:51 -0700

    Add utility to get all node definitions for diffs

---
+++ Assets/UdonSharp/Editor/Editors/GrabNodeDefinitions.cs
@@ -0,0 +1,28 @@
+﻿#if UNITY_EDITOR && UDONSHARP_DEBUG
+
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEditor;
+using UnityEngine;
+using VRC.Udon.Editor;
+
+public class GrabNodeDefinitions : EditorWindow
+{
+    [MenuItem("Window/Udon Sharp/Node Definition Grabber")]
+    static void Init()
+    {
+        GrabNodeDefinitions window = GetWindow<GrabNodeDefinitions>(false, "Node Definition Grabber");
+    }
+
+    private void OnGUI()
+    {
+        if (GUILayout.Button("Get Node Names"))
+        {
+            IEnumerable<string> nodeNames = UdonEditorManager.Instance.GetNodeDefinitions().Select(e => e.fullName).OrderBy(e => e);
+            EditorGUIUtility.systemCopyBuffer = string.Join("\n", nodeNames);
+        }
+    }
+}
+
+#endif
