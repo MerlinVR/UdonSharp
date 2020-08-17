4c61a6bfd6912701d32e29e744f3c7d949486f57
Merlin <36685500+Merlin-san@users.noreply.github.com>
2020-08-17 02:39:27 -0700

    Add custom inspector for the IntegrationTestRunner

--- Assets/UdonSharp/Tests/TestLib/IntegrationTests/IntegrationTestRunner.cs
+++ Assets/UdonSharp/Tests/TestLib/IntegrationTests/IntegrationTestRunner.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 using VRC.SDKBase;
 using VRC.Udon;
 
+#if !COMPILER_UDONSHARP && UNITY_EDITOR
+using UnityEditor;
+using UnityEditorInternal;
+using UdonSharpEditor;
+#endif
+
 namespace UdonSharp.Tests
 {
     [AddComponentMenu("Udon Sharp/Test Lib/Integration Test Runner")]
@@ -44,4 +50,44 @@ namespace UdonSharp.Tests
             Debug.Log($"[<color=#00AF54>UdonSharp Tests</color>] Tests finished [{passedTests}/{totalTests}]");
         }
     }
+
+#if !COMPILER_UDONSHARP && UNITY_EDITOR
+    [CustomEditor(typeof(IntegrationTestRunner))]
+    public class IntegrationTestRunnerInspector : Editor
+    {
+        ReorderableList testList;
+
+        private void OnEnable()
+        {
+            //if (target == null) // Hack I need to figure out why this is breaking further down
+            //    return;
+
+            testList = new ReorderableList(serializedObject, serializedObject.FindProperty("integrationTests"), true, true, true, true);
+            testList.drawElementCallback = (Rect rect, int index, bool isActive, bool isFocused) =>
+            {
+                EditorGUI.PropertyField(new Rect(rect.x, rect.y + 2, rect.width, EditorGUIUtility.singleLineHeight), testList.serializedProperty.GetArrayElementAtIndex(index), label: new GUIContent());
+            };
+            testList.drawHeaderCallback = (Rect rect) => { EditorGUI.LabelField(rect, "Integration Tests"); };
+        }
+
+        private void OnDisable()
+        {
+            testList = null;
+        }
+
+        public override void OnInspectorGUI()
+        {
+            if (UdonSharpGUI.DrawDefaultUdonSharpBehaviourHeader(target)) return;
+
+            EditorGUILayout.PropertyField(serializedObject.FindProperty("runTestOnStart"));
+            EditorGUILayout.PropertyField(serializedObject.FindProperty("logPassedTests"));
+
+            EditorGUILayout.Space();
+
+            testList.DoLayoutList();
+
+            serializedObject.ApplyModifiedProperties();
+        }
+    }
+#endif
 }
