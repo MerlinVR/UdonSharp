445549edab1bcab0eda34d0ca4ab789cf4898f8e
Merlin <36685500+Merlin-san@users.noreply.github.com>
2020-08-24 12:54:51 -0700

    Add HeapInspector script for testing

---
+++ Assets/UdonSharp/Tests/TestScripts/Core/Editor/HeapInspector.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UdonSharpEditor;
+using UnityEditor;
+using UnityEngine;
+
+namespace UdonSharp.Tests
+{
+    [CustomEditor(typeof(DefaultHeapValueTest))]
+    public class DefaultHeapValueTestInspector : Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            if (UdonSharpGUI.DrawDefaultUdonSharpBehaviourHeader(target)) return;
+
+            DefaultHeapValueTest heapValueTest = (DefaultHeapValueTest)target;
+
+            EditorGUI.BeginChangeCheck();
+            int newVal = EditorGUILayout.IntField("AAA field", (int)(heapValueTest.objectIntVal ?? 0));
+
+            if (EditorGUI.EndChangeCheck())
+                Undo.RecordObject(target, "Change aaa field");
+
+            heapValueTest.objectIntVal = newVal;
+            heapValueTest.syncedObjectIntVal = (int)heapValueTest.objectIntVal;
+        }
+    }
+}
