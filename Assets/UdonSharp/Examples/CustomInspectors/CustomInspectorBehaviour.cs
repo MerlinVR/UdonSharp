322fa769caa8a0626f008e3e943423b3c61df460
Merlin <36685500+Merlin-san@users.noreply.github.com>
2020-09-01 15:52:54 -0700

    Update CustomInspectorBehaviour
    - Make it more decent as a basic example

--- Assets/UdonSharp/Examples/CustomInspectors/CustomInspectorBehaviour.cs
+++ Assets/UdonSharp/Examples/CustomInspectors/CustomInspectorBehaviour.cs
@@ -4,42 +4,50 @@ using VRC.SDK3.Components;
 using VRC.SDKBase;
 using VRC.Udon;
 
+#if !COMPILER_UDONSHARP && UNITY_EDITOR // These using statements must be wrapped in this check to prevent issues on builds
+using UnityEditor;
+using UdonSharpEditor;
+#endif
+
 namespace UdonSharp.Examples.Inspectors
 {
     /// <summary>
     /// Example behaviour that has a custom inspector
     /// </summary>
-    /// 
     public class CustomInspectorBehaviour : UdonSharpBehaviour 
     {
-        public GameObject[] gameObjects;
+        public string stringVal;
 
-        public GameObject refGameObject;
-
-        public Vector3 behaviourPos;
-         
-        //public float randomFloat;
-
-        private void Update() 
+        private void Update()
         {
-            string gameObjectNames2 = "Names: " + string.Join(", ", (object[])gameObjects);
-            //string gameObjectNames = "Names: ";
-            //foreach (GameObject obj in gameObjects)
-            //{
-            //    if (obj)
-            //        gameObjectNames += obj.ToString() + ", ";
-            //    else
-            //        gameObjectNames += "null, ";
-            //}
-
-            //Debug.Log(gameObjectNames);
-            //Debug.Log(gameObjectNames2);
-            //Debug.Log(gameObjects);
-            //Debug.Log(refGameObject);
-
-            transform.position = behaviourPos; 
-
-            //randomFloat = Random.Range(0f, 100f);
+            Debug.Log($"CustomInspectorBehaviour: {stringVal}");
         }
     }
+
+    // Editor scripts must be wrapped in a UNITY_EDITOR check to prevent issues while uploading worlds. The !COMPILER_UDONSHARP check prevents UdonSharp from throwing errors about unsupported code here.
+#if !COMPILER_UDONSHARP && UNITY_EDITOR 
+    [CustomEditor(typeof(CustomInspectorBehaviour))]
+    public class CustomInspectorEditor : Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            // Draws the default convert to UdonBehaviour button, program asset field, sync settings, etc.
+            if (UdonSharpGUI.DrawDefaultUdonSharpBehaviourHeader(target)) return;
+
+            CustomInspectorBehaviour inspectorBehaviour = (CustomInspectorBehaviour)target;
+
+            EditorGUI.BeginChangeCheck();
+
+            // A simple string field modification with Undo handling
+            string newStrVal = EditorGUILayout.TextField("String Val", inspectorBehaviour.stringVal);
+
+            if (EditorGUI.EndChangeCheck())
+            {
+                Undo.RecordObject(inspectorBehaviour, "Modify string val");
+
+                inspectorBehaviour.stringVal = newStrVal;
+            }
+        }
+    }
+#endif
 }
