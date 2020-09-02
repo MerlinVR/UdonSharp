6f6164570fb44d7e211f34e2dde7e78bdba71258
Merlin <36685500+Merlin-san@users.noreply.github.com>
2020-09-02 01:09:38 -0700

    Extend the custom inspector example
    - Now includes using the Handles API, creating and deleting UdonSharpBehaviours, and using OnDrawGizmosSelected

--- Assets/UdonSharp/Examples/CustomInspectors/CustomInspectorBehaviour.cs
+++ Assets/UdonSharp/Examples/CustomInspectors/CustomInspectorBehaviour.cs
@@ -1,12 +1,10 @@
 
 using UnityEngine;
-using VRC.SDK3.Components;
-using VRC.SDKBase;
-using VRC.Udon;
 
 #if !COMPILER_UDONSHARP && UNITY_EDITOR // These using statements must be wrapped in this check to prevent issues on builds
 using UnityEditor;
 using UdonSharpEditor;
+using System.Linq;
 #endif
 
 namespace UdonSharp.Examples.Inspectors
@@ -14,14 +12,14 @@ namespace UdonSharp.Examples.Inspectors
     /// <summary>
     /// Example behaviour that has a custom inspector
     /// </summary>
+    [AddComponentMenu("Udon Sharp/Inspectors/Custom Inspector Behaviour")]
     public class CustomInspectorBehaviour : UdonSharpBehaviour 
     {
         public string stringVal;
 
-        private void Update()
-        {
-            Debug.Log($"CustomInspectorBehaviour: {stringVal}");
-        }
+        public Vector3 orbitOrigin = new Vector3(0, 3f, 0);
+
+        public CustomInspectorChildBehaviour[] childBehaviours;
     }
 
     // Editor scripts must be wrapped in a UNITY_EDITOR check to prevent issues while uploading worlds. The !COMPILER_UDONSHARP check prevents UdonSharp from throwing errors about unsupported code here.
@@ -47,6 +45,77 @@ namespace UdonSharp.Examples.Inspectors
 
                 inspectorBehaviour.stringVal = newStrVal;
             }
+
+            EditorGUI.BeginDisabledGroup(true);
+
+            EditorGUILayout.Vector3Field("Orbit Origin", inspectorBehaviour.orbitOrigin); // Set via handles in OnSceneGUI
+
+            EditorGUI.EndDisabledGroup();
+
+            EditorGUILayout.Space();
+
+            DrawChildBehaviourControls(inspectorBehaviour);
+        }
+
+        void DrawChildBehaviourControls(CustomInspectorBehaviour inspectorBehaviour)
+        {
+            EditorGUILayout.LabelField($"Child count: {inspectorBehaviour.childBehaviours.Length}");
+
+            if (GUILayout.Button("Add child behaviour"))
+            {
+                GameObject newGameObject = new GameObject("Custom Inspector Child");
+                Undo.RegisterCreatedObjectUndo(newGameObject, "Create child behaviour");
+                newGameObject.transform.SetParent(inspectorBehaviour.transform);
+
+                CustomInspectorChildBehaviour newChild = newGameObject.AddUdonSharpComponent<CustomInspectorChildBehaviour>();
+
+                newChild.parentBehaviour = inspectorBehaviour;
+                newChild.myIndex = inspectorBehaviour.childBehaviours.Length;
+
+                Undo.RecordObject(inspectorBehaviour, "Add child behaviour");
+                inspectorBehaviour.childBehaviours = inspectorBehaviour.childBehaviours.Append(newChild).ToArray();
+
+                // Will recursively apply the child behaviour as well since it is referenced in the childBehaviours array
+                inspectorBehaviour.ApplyProxyModifications();
+            }
+
+            if (GUILayout.Button("Remove child behaviour"))
+            {
+                if (inspectorBehaviour.childBehaviours.Length > 0)
+                {
+                    CustomInspectorChildBehaviour child = inspectorBehaviour.childBehaviours.Last();
+
+                    Undo.RecordObject(inspectorBehaviour, "Delete child object");
+                    inspectorBehaviour.childBehaviours = inspectorBehaviour.childBehaviours.Take(inspectorBehaviour.childBehaviours.Length - 1).ToArray();
+
+                    Undo.DestroyObjectImmediate(child.gameObject);
+                }
+            }
+        }
+
+        private void OnSceneGUI()
+        {
+            CustomInspectorBehaviour inspectorBehaviour = (CustomInspectorBehaviour)target;
+
+            EditorGUI.BeginChangeCheck();
+            Vector3 newTarget = Handles.PositionHandle(inspectorBehaviour.orbitOrigin, Quaternion.identity);
+            Handles.Label(newTarget, "Orbit Origin");
+
+            if (EditorGUI.EndChangeCheck())
+            {
+                Undo.RecordObject(inspectorBehaviour, "Move orbit origin");
+                inspectorBehaviour.orbitOrigin = newTarget;
+            }
+
+            Color oldColor = Handles.color;
+            Handles.color = Color.red;
+
+            foreach (CustomInspectorChildBehaviour child in inspectorBehaviour.childBehaviours)
+            {
+                Handles.DrawWireDisc(child.transform.position, Vector3.up, 0.2f);
+            }
+
+            Handles.color = oldColor;
         }
     }
 #endif
---
+++ Assets/UdonSharp/Examples/CustomInspectors/CustomInspectorChildBehaviour.cs
@@ -0,0 +1,43 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+#if !COMPILER_UDONSHARP && UNITY_EDITOR
+using UdonSharpEditor;
+#endif
+
+namespace UdonSharp.Examples.Inspectors
+{
+    [AddComponentMenu("")]
+    public class CustomInspectorChildBehaviour : UdonSharpBehaviour
+    {
+        public CustomInspectorBehaviour parentBehaviour;
+        public int myIndex;
+
+        float sinHeight;
+
+        private void Update()
+        {
+            int totalChildCount = parentBehaviour.childBehaviours.Length;
+
+            float orbitAmount = (myIndex / (float)totalChildCount) * Mathf.PI * 2 + Time.time * 0.4f; // 0.4 rotations per second
+
+            transform.position = parentBehaviour.orbitOrigin + new Vector3(Mathf.Sin(orbitAmount), 0f, Mathf.Cos(orbitAmount));
+
+            sinHeight = Mathf.Sin(orbitAmount);
+        }
+
+#if !COMPILER_UDONSHARP && UNITY_EDITOR
+        private void OnDrawGizmosSelected()
+        {
+            // RootOnly update will only copy the data for this behaviour from Udon to the proxy
+            this.UpdateProxy(ProxySerializationPolicy.RootOnly);
+
+            Gizmos.DrawLine(transform.position, transform.position + Vector3.up * sinHeight * 0.6f);
+            Gizmos.DrawCube(transform.position, Vector3.one * 0.2f);
+        }
+#endif
+    }
+}
