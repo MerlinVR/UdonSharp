ca94db8f5dfe92a20f37bf370d3ed3eeb53465a1
ChanyaKushima <chanya.vrchat@gmail.com>
2021-07-18 22:48:44 +0900

    Add test for d9f5b6b.

---
+++ Assets/UdonSharp/Tests/TestScripts/Core/PropertyTestReferenceScript.cs
@@ -0,0 +1,18 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+namespace UdonSharp.Tests
+{
+    [AddComponentMenu("Udon Sharp/Tests/UserPropertyTest")]
+
+    public class PropertyTestReferenceScript : UdonSharpBehaviour
+    {
+        [System.NonSerialized]
+        public int _value = 1;
+        public int Value { get => _value; set => _value = value; }
+        public int GetValue() => _value;
+    }
+}
