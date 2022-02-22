MerlinVR/UdonSharp

commit a61d29793c94e85445c7fb1b7f152eb4facee746

Add handling for prefab instances with delta on unityengine.Object array
If a prefab instance in a scene has not been upgraded and has no delta on its string data, but a delta on its UnityEngine.Object array, it could get caught in an intermediate state where it's not upgraded fully. This adds handling to prevent that from being an issue.

Assets/UdonSharp/Editor/UdonSharpEditorUtility.cs
@@ -172,11 +172,13 @@ namespace UdonSharpEditor
 
         private const string UDONSHARP_BEHAVIOUR_VERSION_KEY = "___UdonSharpBehaviourVersion___";
         private const string UDONSHARP_BEHAVIOUR_UPGRADE_MARKER = "___UdonSharpBehaviourPersistDataFromUpgrade___";
+        private const string UDONSHARP_SCENE_BEHAVIOUR_UPGRADE_MARKER = "___UdonSharpBehaviourHasDoneSceneUpgrade___";
 
         private static bool ShouldPersistVariable(string variableSymbol)
         {
             return variableSymbol == UDONSHARP_BEHAVIOUR_VERSION_KEY ||
-                   variableSymbol == UDONSHARP_BEHAVIOUR_UPGRADE_MARKER;
+                   variableSymbol == UDONSHARP_BEHAVIOUR_UPGRADE_MARKER ||
+                   variableSymbol == UDONSHARP_SCENE_BEHAVIOUR_UPGRADE_MARKER;
         }
 
         internal static UdonSharpBehaviourVersion GetBehaviourVersion(UdonBehaviour behaviour)
@@ -212,7 +214,7 @@ namespace UdonSharpEditor
             UdonSharpUtils.LogError("Could not set version variable");
         }
 
-        internal static bool BehaviourRequiresBackwardsCompatibilityPersistence(UdonBehaviour behaviour)
+        private static bool BehaviourRequiresBackwardsCompatibilityPersistence(UdonBehaviour behaviour)
         {
             if (behaviour.publicVariables.TryGetVariableValue<bool>(UDONSHARP_BEHAVIOUR_UPGRADE_MARKER, out bool needsBackwardsCompat) && PrefabUtility.IsPartOfPrefabAsset(behaviour))
                 return needsBackwardsCompat;
@@ -222,7 +224,7 @@ namespace UdonSharpEditor
 
         internal static void ClearBehaviourVariables(UdonBehaviour behaviour, bool clearPersistentVariables = false)
         {
-            foreach (string publicVarSymbol in behaviour.publicVariables.VariableSymbols.ToArray())
+            foreach (string publicVarSymbol in behaviour.publicVariables.VariableSymbols.ToArray()) // ToArray so we don't modify the collection while iterating it
             {
                 if (!clearPersistentVariables && ShouldPersistVariable(publicVarSymbol))
                     continue;
@@ -244,8 +246,28 @@ namespace UdonSharpEditor
                 behaviour.publicVariables.TryAddVariable(newVar);
             }
             
-            EditorUtility.SetDirty(behaviour);
-            PrefabUtility.RecordPrefabInstancePropertyModifications(behaviour);
+            UdonSharpUtils.SetDirty(behaviour);
+        }
+
+        private static void SetSceneBehaviourUpgraded(UdonBehaviour behaviour)
+        {
+            if (!PrefabUtility.IsPartOfPrefabInstance(behaviour))
+                return;
+            
+            if (!behaviour.publicVariables.TrySetVariableValue<bool>(UDONSHARP_SCENE_BEHAVIOUR_UPGRADE_MARKER, true))
+            {
+                behaviour.publicVariables.RemoveVariable(UDONSHARP_SCENE_BEHAVIOUR_UPGRADE_MARKER);
+                
+                IUdonVariable newVar = new UdonVariable<bool>(UDONSHARP_SCENE_BEHAVIOUR_UPGRADE_MARKER, true);
+                behaviour.publicVariables.TryAddVariable(newVar);
+            }
+            
+            UdonSharpUtils.SetDirty(behaviour);
+        }
+
+        private static bool HasSceneBehaviourUpgradeFlag(UdonBehaviour behaviour)
+        {
+            return behaviour.publicVariables.TryGetVariableValue<bool>(UDONSHARP_SCENE_BEHAVIOUR_UPGRADE_MARKER, out bool sceneBehaviourUpgraded) && sceneBehaviourUpgraded;
         }
         
         /// <summary>
@@ -448,9 +470,35 @@ namespace UdonSharpEditor
             // Copy data over from UdonBehaviour to UdonSharpBehaviour
             foreach (UdonBehaviour udonBehaviour in behaviours)
             {
-                if (!IsUdonSharpBehaviour(udonBehaviour) || 
-                    GetBehaviourVersion(udonBehaviour) != UdonSharpBehaviourVersion.V0DataUpgradeNeeded)
+                if (!IsUdonSharpBehaviour(udonBehaviour))
                     continue;
+                
+                bool needsPrefabInstanceUpgrade = false;
+
+                if (GetBehaviourVersion(udonBehaviour) != UdonSharpBehaviourVersion.V0DataUpgradeNeeded)
+                {
+                    // Check if the prefab instance has a prefab that was upgraded causing the string data to be copied, but has a delta'd UnityEngine.Object storage array
+                    if (PrefabUtility.IsPartOfPrefabInstance(udonBehaviour) &&
+                        !HasSceneBehaviourUpgradeFlag(udonBehaviour))
+                    {
+                        UdonBehaviour prefabSource = PrefabUtility.GetCorrespondingObjectFromSource(udonBehaviour);
+
+                        if (prefabSource && BehaviourRequiresBackwardsCompatibilityPersistence(prefabSource))
+                        {
+                            PropertyModification[] modifications =
+                                PrefabUtility.GetPropertyModifications(udonBehaviour);
+
+                            if (modifications != null &&
+                                modifications.Any(e => e.propertyPath.StartsWith("publicVariablesUnityEngineObjects", StringComparison.Ordinal)))
+                            {
+                                needsPrefabInstanceUpgrade = true;
+                            }
+                        }
+                    }
+
+                    if (!needsPrefabInstanceUpgrade)
+                        continue;
+                }
                 
                 UdonSharpBehaviour proxy = GetProxyBehaviour(udonBehaviour);
                 
@@ -461,7 +509,13 @@ namespace UdonSharpEditor
                 ClearBehaviourVariables(udonBehaviour, true);
                             
                 SetBehaviourVersion(udonBehaviour, UdonSharpBehaviourVersion.V1);
-                
+
+                if (needsPrefabInstanceUpgrade)
+                {
+                    SetSceneBehaviourUpgraded(udonBehaviour);
+                    UdonSharpUtils.Log($"Scene behaviour {udonBehaviour.name} needed UnityEngine.Object upgrade pass");
+                }
+
                 UdonSharpUtils.SetDirty(proxy);
                 
                 UdonSharpUtils.Log($"Upgraded scene behaviour {udonBehaviour.name}", udonBehaviour);
@@ -631,7 +685,6 @@ namespace UdonSharpEditor
         /// Finds an existing proxy behaviour, if none exists returns null
         /// </summary>
         /// <param name="udonBehaviour"></param>
-        /// <param name="proxySerializationPolicy"></param>
         /// <returns></returns>
         private static UdonSharpBehaviour FindProxyBehaviour_Internal(UdonBehaviour udonBehaviour)
         {
@@ -953,7 +1006,7 @@ namespace UdonSharpEditor
             if (visitedSet.Contains(rootObject))
                 return;
 
-            System.Type objectType = rootObject.GetType();
+            Type objectType = rootObject.GetType();
 
             if (objectType.IsValueType)
                 return;
@@ -971,7 +1024,7 @@ namespace UdonSharpEditor
 
             if (objectType.IsArray)
             {
-                foreach (object arrayElement in (System.Array)rootObject)
+                foreach (object arrayElement in (Array)rootObject)
                 {
                     CollectUdonSharpBehaviourReferencesInternal(arrayElement, gatheredSet, visitedSet);
                 }
@@ -1019,14 +1072,14 @@ namespace UdonSharpEditor
                     // This is an absolute mess, it should probably just be simplified to counting the number of affected behaviours
                     string referencedBehaviourStr;
                     if (allReferencedBehaviours.Count <= 2)
-                        referencedBehaviourStr = string.Join(", ", allReferencedBehaviours.Select(e => $"'{e.ToString()}'"));
+                        referencedBehaviourStr = string.Join(", ", allReferencedBehaviours.Select(e => $"'{e}'"));
                     else
                         referencedBehaviourStr = $"{allReferencedBehaviours.Count} behaviours";
 
                     string rootBehaviourStr;
 
                     if (components.Length <= 2)
-                        rootBehaviourStr = $"{string.Join(", ", components.Select(e => $"'{e.ToString()}'"))} reference{(components.Length == 1 ? "s" : "")} ";
+                        rootBehaviourStr = $"{string.Join(", ", components.Select(e => $"'{e}'"))} reference{(components.Length == 1 ? "s" : "")} ";
                     else
                         rootBehaviourStr = $"{components.Length} behaviours to convert reference ";
 
@@ -1094,12 +1147,7 @@ namespace UdonSharpEditor
 
                 GameObject targetGameObject = targetObject.gameObject;
 
-                UdonBehaviour udonBehaviour = null;
-
-                if (shouldUndo)
-                    udonBehaviour = Undo.AddComponent<UdonBehaviour>(targetGameObject);
-                else
-                    udonBehaviour = targetGameObject.AddComponent<UdonBehaviour>();
+                UdonBehaviour udonBehaviour = shouldUndo ? Undo.AddComponent<UdonBehaviour>(targetGameObject) : targetGameObject.AddComponent<UdonBehaviour>();
 
                 udonBehaviour.programSource = programAsset;
 #pragma warning disable CS0618 // Type or member is obsolete

Assets/UdonSharp/Editor/Editors/UdonSharpBehaviourEditor.cs
@@ -12,6 +12,7 @@ using UnityEngine;
 using UnityEngine.UIElements;
 using VRC.Udon;
 using VRC.Udon.Editor;
+using Object = UnityEngine.Object;
 
 #if ODIN_INSPECTOR_3
 using UdonSharpEditor;
@@ -137,7 +138,7 @@ namespace UdonSharpEditor
             errorState = BehaviourInspectorErrorState.Success;
             bool needsUpgradePass = false;
             
-            foreach (var target in targets)
+            foreach (Object target in targets)
             {
                 UdonBehaviour targetBehaviour = (UdonBehaviour)target;
 
@@ -148,24 +149,24 @@ namespace UdonSharpEditor
                     break;
                 }
 
-                if (UdonSharpEditorUtility.GetProxyBehaviour(targetBehaviour) == null)
-                {
-                    needsUpgradePass = true;
+                if (UdonSharpEditorUtility.GetProxyBehaviour(targetBehaviour) != null) 
+                    continue;
+                
+                needsUpgradePass = true;
                     
-                    if (PrefabUtility.IsPartOfPrefabInstance(targetBehaviour) &&
-                        !PrefabUtility.IsAddedComponentOverride(targetBehaviour))
-                    {
-                        UdonSharpUtils.LogWarning($"UdonBehaviour '{targetBehaviour}' needs upgrade on source prefab asset.", targetBehaviour);
-                        errorState = BehaviourInspectorErrorState.PrefabNeedsUpgrade;
-                        break;
-                    }
+                if (PrefabUtility.IsPartOfPrefabInstance(targetBehaviour) &&
+                    !PrefabUtility.IsAddedComponentOverride(targetBehaviour))
+                {
+                    UdonSharpUtils.LogWarning($"UdonBehaviour '{targetBehaviour}' needs upgrade on source prefab asset.", targetBehaviour);
+                    errorState = BehaviourInspectorErrorState.PrefabNeedsUpgrade;
+                    break;
                 }
             }
 
             if (!needsUpgradePass || errorState != BehaviourInspectorErrorState.Success)
                 return;
 
-            foreach (var target in targets)
+            foreach (Object target in targets)
             {
                 UdonBehaviour targetBehaviour = (UdonBehaviour)target;
                 UdonSharpBehaviour udonSharpBehaviour = UdonSharpEditorUtility.GetProxyBehaviour(targetBehaviour);
@@ -182,6 +183,7 @@ namespace UdonSharpEditor
                         UdonSharpEditorUtility.SetBackingUdonBehaviour(udonSharpBehaviour, targetBehaviour);
                         UdonSharpEditorUtility.MoveComponentRelativeToComponent(udonSharpBehaviour, targetBehaviour, true);
                         UdonSharpEditorUtility.SetBehaviourVersion(targetBehaviour, UdonSharpBehaviourVersion.CurrentVersion);
+                        UdonSharpEditorUtility.SetSceneBehaviourUpgraded(targetBehaviour);
                     }
                     finally
                     {

commit 9fe941b269289cc2c9b5c5c241e3ef842f344e22

Fix issues with upgrading introduced by a61d297
Wasn't flagging behaviours and causing them to be double upgraded

Assets/UdonSharp/Editor/UdonSharpEditorUtility.cs
@@ -475,19 +475,20 @@ namespace UdonSharpEditor
                 
                 bool needsPrefabInstanceUpgrade = false;
 
-                if (GetBehaviourVersion(udonBehaviour) != UdonSharpBehaviourVersion.V0DataUpgradeNeeded)
+                UdonSharpBehaviourVersion behaviourVersion = GetBehaviourVersion(udonBehaviour);
+                if (behaviourVersion >= UdonSharpBehaviourVersion.V1)
                 {
                     // Check if the prefab instance has a prefab that was upgraded causing the string data to be copied, but has a delta'd UnityEngine.Object storage array
                     if (PrefabUtility.IsPartOfPrefabInstance(udonBehaviour) &&
                         !HasSceneBehaviourUpgradeFlag(udonBehaviour))
                     {
                         UdonBehaviour prefabSource = PrefabUtility.GetCorrespondingObjectFromSource(udonBehaviour);
-
+                    
                         if (prefabSource && BehaviourRequiresBackwardsCompatibilityPersistence(prefabSource))
                         {
                             PropertyModification[] modifications =
                                 PrefabUtility.GetPropertyModifications(udonBehaviour);
-
+                    
                             if (modifications != null &&
                                 modifications.Any(e => e.propertyPath.StartsWith("publicVariablesUnityEngineObjects", StringComparison.Ordinal)))
                             {
@@ -509,12 +510,10 @@ namespace UdonSharpEditor
                 ClearBehaviourVariables(udonBehaviour, true);
                             
                 SetBehaviourVersion(udonBehaviour, UdonSharpBehaviourVersion.V1);
+                SetSceneBehaviourUpgraded(udonBehaviour);
 
                 if (needsPrefabInstanceUpgrade)
-                {
-                    SetSceneBehaviourUpgraded(udonBehaviour);
-                    UdonSharpUtils.Log($"Scene behaviour {udonBehaviour.name} needed UnityEngine.Object upgrade pass");
-                }
+                    UdonSharpUtils.Log($"Scene behaviour {udonBehaviour.name} needed UnityEngine.Object upgrade pass", udonBehaviour);
 
                 UdonSharpUtils.SetDirty(proxy);
                 

Assets/UdonSharp/Editor/UdonSharpProgramAsset.cs
@@ -46,7 +46,7 @@ namespace UdonSharp
         public MonoScript sourceCsScript;
 
         [SerializeField]
-        private UdonSharpProgramVersion scriptVersion = UdonSharpProgramVersion.CurrentVersion;
+        private UdonSharpProgramVersion scriptVersion = UdonSharpProgramVersion.Unknown;
 
         /// <summary>
         /// The version the attached C# script is on, determines if this needs an upgrade pass that rewrites the script file.

Assets/UdonSharp/Editor/UdonSharpUtils.cs
@@ -480,9 +480,7 @@ namespace UdonSharp
         internal static void SetDirty(UnityEngine.Object obj)
         {
             EditorUtility.SetDirty(obj);
-            
-            if (PrefabUtility.IsPartOfPrefabInstance(obj))
-                PrefabUtility.RecordPrefabInstancePropertyModifications(obj);
+            PrefabUtility.RecordPrefabInstancePropertyModifications(obj);
         }
 
         private static PropertyInfo _getLoadedAssembliesProp;