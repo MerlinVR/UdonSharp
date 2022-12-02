MerlinVR/UdonSharp

commit 984f4d0140a7183ba16ef968fb883671779e3823

Fix IsUdonEvent() to also check parameter equivalence
- This caused issues if people attempted to declare a method with the same name as a built-in method, but with different parameters

Packages/com.vrchat.UdonSharp/Editor/Compiler/CompilationContext.cs
@@ -358,7 +358,7 @@ namespace UdonSharp.Compiler
             string[] paramNames = new string[methodSymbol.Parameters.Length];
             string returnName = null;
             
-            if (CompilerUdonInterface.IsUdonEvent(methodName))
+            if (CompilerUdonInterface.IsUdonEvent(methodSymbol))
             {
                 var paramArgs = CompilerUdonInterface.GetUdonEventArgs(methodName);
                 methodName = CompilerUdonInterface.GetUdonEventName(methodName);

Packages/com.vrchat.UdonSharp/Editor/Compiler/Udon/CompilerUdonInterface.cs
@@ -233,10 +233,19 @@ namespace UdonSharp.Compiler.Udon
             return ExternAssemblySet.Contains(type.Assembly);
         }
 
-        public static bool IsUdonEvent(string eventName)
+        public static bool IsUdonEvent(MethodSymbol method)
         {
             CacheInit();
-            return _builtinEventLookup.ContainsKey(eventName);
+            
+            // ReSharper disable once InvokeAsExtensionMethod
+            if (_builtinEventLookup.ContainsKey(method.Name) &&
+                !method.Parameters.Any(e => e.IsByRef) && // Builtin events should never have out/ref params
+                Enumerable.SequenceEqual(method.Parameters.Select(e => e.Type.UdonType.SystemType), GetUdonEventArgs(method.Name).Select(e => e.Item2)))
+            {
+                return true;
+            }
+
+            return false;
         }
 
         public static string GetUdonEventName(string eventName)

Packages/com.vrchat.UdonSharp/Editor/Compiler/Binder/BoundNodes/BoundInvocationExpression.cs
@@ -408,7 +408,7 @@ namespace UdonSharp.Compiler.Binder
             
             if (symbol.IsExtern)
             {
-                if (CompilerUdonInterface.IsUdonEvent(symbol.Name) &&
+                if (CompilerUdonInterface.IsUdonEvent(symbol) &&
                     symbol.ContainingType == context.GetTypeSymbol(typeof(UdonSharpBehaviour))) // Pass through for making base calls on the U# behaviour type return noop
                     return new BoundUdonSharpBehaviourInvocationExpression(node, symbol, instanceExpression, parameterExpressions);
 

Packages/com.vrchat.UdonSharp/Editor/Compiler/Binder/Symbols/UdonSharpBehaviourMethodSymbol.cs
@@ -16,8 +16,6 @@ namespace UdonSharp.Compiler.Symbols
     {
         public ExportAddress ExportedMethodAddress { get; }
         
-        public bool IsUdonEvent { get; }
-        
         public bool NeedsExportFromReference { get; private set; }
 
         /// <summary>
@@ -29,7 +27,6 @@ namespace UdonSharp.Compiler.Symbols
         public UdonSharpBehaviourMethodSymbol(IMethodSymbol sourceSymbol, AbstractPhaseContext context)
             : base(sourceSymbol, context)
         {
-            IsUdonEvent = CompilerUdonInterface.IsUdonEvent(sourceSymbol.Name);
             ExportedMethodAddress = new ExportAddress(ExportAddress.AddressKind.String, this);
         }
 

Packages/com.vrchat.UdonSharp/Editor/Compiler/Emit/EmitContext.cs
@@ -780,7 +780,7 @@ namespace UdonSharp.Compiler.Emit
                 return false;
             
             return methodSymbol.RoslynSymbol.DeclaredAccessibility == Accessibility.Public ||
-                   CompilerUdonInterface.IsUdonEvent(methodSymbol.Name) ||
+                   CompilerUdonInterface.IsUdonEvent(methodSymbol) ||
                    (methodSymbol is UdonSharpBehaviourMethodSymbol udonSharpBehaviourMethodSymbol && udonSharpBehaviourMethodSymbol.NeedsExportFromReference);
         }
 

commit 99c1a512f0c4caadf128994308d747ea457cf635

Avoid some breakages that could be caused by 984f4d0140a7183ba16ef968fb883671779e3823
- Try to avoid this fix being a breaking change for people who omit arguments on builtin events, while not strictly recommended. This is also partially mirroring Unity behavior where Unity will allow you to declare stuff like OnTriggerEnter() without any parameters and still fire it. Unity will also prevent declaration of event-named functions that have different parameter types so we add validation for that here as well, since existing uses of events with the same name but different parameters would've been errant and liable to breaking in weird ways anyways.

Packages/com.vrchat.UdonSharp/Editor/Compiler/CompilationContext.cs
@@ -12,6 +12,7 @@ using UdonSharp.Compiler.Assembly;
 using UdonSharp.Compiler.Binder;
 using UdonSharp.Compiler.Symbols;
 using UdonSharp.Compiler.Udon;
+using UdonSharp.Core;
 using UdonSharpEditor;
 using UnityEditor;
 using UnityEditor.Compilation;
@@ -357,13 +358,19 @@ namespace UdonSharp.Compiler
             string methodName = methodSymbol.Name;
             string[] paramNames = new string[methodSymbol.Parameters.Length];
             string returnName = null;
+
+            if (!methodSymbol.IsStatic && !CompilerUdonInterface.IsUdonEvent(methodSymbol) && CompilerUdonInterface.IsUdonEventName(methodName))
+            {
+                // If the user has declared an event with the same name as a built-in one but with different arguments, we imitate Unity here and complain for our built-in events
+                throw new CompilerException($"Method with same name as built-in event '{methodSymbol}' cannot be declared with parameter types that do not match the event.", methodSymbol.RoslynSymbol.DeclaringSyntaxReferences.FirstOrDefault()?.GetSyntax()?.GetLocation());
+            }
             
             if (CompilerUdonInterface.IsUdonEvent(methodSymbol))
             {
-                var paramArgs = CompilerUdonInterface.GetUdonEventArgs(methodName);
+                ImmutableArray<(string, Type)> paramArgs = CompilerUdonInterface.GetUdonEventArgs(methodName);
                 methodName = CompilerUdonInterface.GetUdonEventName(methodName);
 
-                for (int i = 0; i < paramNames.Length; ++i)
+                for (int i = 0; i < paramNames.Length && i < paramArgs.Length; ++i)
                     paramNames[i] = paramArgs[i].Item1;
             }
             else

Packages/com.vrchat.UdonSharp/Editor/Compiler/Udon/CompilerUdonInterface.cs
@@ -233,16 +233,26 @@ namespace UdonSharp.Compiler.Udon
             return ExternAssemblySet.Contains(type.Assembly);
         }
 
+        public static bool IsUdonEventName(string name)
+        {
+            CacheInit();
+            
+            return _builtinEventLookup.ContainsKey(name);
+        }
+
         public static bool IsUdonEvent(MethodSymbol method)
         {
             CacheInit();
             
             // ReSharper disable once InvokeAsExtensionMethod
             if (_builtinEventLookup.ContainsKey(method.Name) &&
-                !method.Parameters.Any(e => e.IsByRef) && // Builtin events should never have out/ref params
-                Enumerable.SequenceEqual(method.Parameters.Select(e => e.Type.UdonType.SystemType), GetUdonEventArgs(method.Name).Select(e => e.Item2)))
+                !method.Parameters.Any(e => e.IsByRef)) // Builtin events should never have out/ref params
             {
-                return true;
+                if (method.Parameters.Length == 0 || // Avoid breaking older programs that omit the argument for these events even though they shouldn't. This also somewhat mirrors Unity's behavior as it will fire an event named OnTriggerEnter() without any parameters.
+                    Enumerable.SequenceEqual(method.Parameters.Select(e => e.Type.UdonType.SystemType), GetUdonEventArgs(method.Name).Select(e => e.Item2)))
+                {
+                    return true;
+                }
             }
 
             return false;