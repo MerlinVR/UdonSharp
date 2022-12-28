e0f2bb6edc60b6c14a8135b1814559f4d994ecaf
Talox <arne@vanderlei.be>
2022-12-28 13:17:28 +0100

    added image loader functions

--- Packages/com.vrchat.UdonSharp/Runtime/UdonSharpBehaviour.cs
+++ Packages/com.vrchat.UdonSharp/Runtime/UdonSharpBehaviour.cs
@@ -219,7 +219,9 @@ namespace UdonSharp
         [PublicAPI] public virtual void OnPlayerCollisionStay(VRC.SDKBase.VRCPlayerApi player) { }
         [PublicAPI] public virtual void OnPlayerParticleCollision(VRC.SDKBase.VRCPlayerApi player) { }
         [PublicAPI] public virtual void OnPlayerRespawn(VRC.SDKBase.VRCPlayerApi player) { }
-        
+        [PublicAPI] public virtual void OnImageLoadSuccess(VRC.SDK3.Image.IVRCImageDownload result) { }
+        [PublicAPI] public virtual void OnImageLoadError(VRC.SDK3.Image.IVRCImageDownload result) { }
+
         [PublicAPI] public virtual void OnPostSerialization(VRC.Udon.Common.SerializationResult result) { }
         [PublicAPI] public virtual bool OnOwnershipRequest(VRC.SDKBase.VRCPlayerApi requestingPlayer, VRC.SDKBase.VRCPlayerApi requestedOwner) => true;
 
